0144bf03cbf931b5354a5bd061181b97decdac16
Hakan Çevik <hakanxcevik7@gmail.com>
2022-06-23 11:20:13 +0300

    Calculator

--- app/src/main/java/com/hakancevik/calculator/MainActivity.java
+++ app/src/main/java/com/hakancevik/calculator/MainActivity.java
@@ -2,91 +2,107 @@ package com.hakancevik.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
+
+import com.hakancevik.calculator.databinding.ActivityMainBinding;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText number1Text;
-    EditText number2Text;
-    TextView textView;
-    int number1,number2,result;
+    private ActivityMainBinding binding;
+
+    double number1, number2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        number1Text = findViewById(R.id.number1Text);
-        number2Text = findViewById(R.id.number2Text);
-        textView = findViewById(R.id.textView);
+
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void sum(View view) {
-        if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            textView.setText("Please, Enter a Number!");
+        if (binding.number1Text.getText().toString().matches("") || binding.number2Text.getText().toString().matches("")) {
+            binding.textView.setText("Please, Enter a Number!");
         }
-        else{
-            number1 = Integer.parseInt(number1Text.getText().toString());
-            number2 = Integer.parseInt(number2Text.getText().toString());
+        else {
+            number1 = Double.parseDouble(binding.number1Text.getText().toString());
+            number2 = Double.parseDouble(binding.number2Text.getText().toString());
 
-            result=number1 + number2;
+            result = number1 + number2;
+            result = Double.parseDouble(new DecimalFormat("##.###").format(result));
 
-            textView.setText("Result = " + result);
+            binding.textView.setText("Result = " + result);
 
         }
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void subtraction(View view) {
-        if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            textView.setText("Please, Enter Number!");
+        if (binding.number1Text.getText().toString().matches("") || binding.number2Text.getText().toString().matches("")) {
+            binding.textView.setText("Please, Enter Number!");
         }
-        else{
-            number1 = Integer.parseInt(number1Text.getText().toString());
-            number2 = Integer.parseInt(number2Text.getText().toString());
+        else {
+            number1 = Double.parseDouble(binding.number1Text.getText().toString());
+            number2 = Double.parseDouble(binding.number2Text.getText().toString());
 
-            result=number1 - number2;
+            result = number1 - number2;
+            result = Double.parseDouble(new DecimalFormat("##.###").format(result));
 
-            textView.setText("Result = "+result);
+            binding.textView.setText("Result = " + result);
 
         }
 
     }
 
-
+    @SuppressLint("SetTextI18n")
     public void multiply(View view) {
 
-        if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            textView.setText("Please, Enter a Number!");
+        if (binding.number1Text.getText().toString().matches("") || binding.number2Text.getText().toString().matches("")) {
+            binding.textView.setText("Please, Enter a Number!");
         }
-        else{
-            number1 = Integer.parseInt(number1Text.getText().toString());
-            number2 = Integer.parseInt(number2Text.getText().toString());
+        else {
+            number1 = Double.parseDouble(binding.number1Text.getText().toString());
+            number2 = Double.parseDouble(binding.number2Text.getText().toString());
 
-            result=number1 * number2;
+            result = number1 * number2;
+            result = Double.parseDouble(new DecimalFormat("##.###").format(result));
 
-            textView.setText("Result = " + result);
+            binding.textView.setText("Result = " + result);
 
         }
 
     }
 
-    public void devide(View view) {
+    @SuppressLint("SetTextI18n")
+    public void divide(View view) {
 
-        if(number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
-            textView.setText("Please, Enter a Number!");
+        if (binding.number1Text.getText().toString().matches("") || binding.number2Text.getText().toString().matches("")) {
+            binding.textView.setText("Please, Enter a Number!");
         }
-        else{
-            number1 = Integer.parseInt(number1Text.getText().toString());
-            number2 = Integer.parseInt(number2Text.getText().toString());
+        else {
+            number1 = Double.parseDouble(binding.number1Text.getText().toString());
+            number2 = Double.parseDouble(binding.number2Text.getText().toString());
 
-            result=number1 / number2;
+            if (number1 != 0 && number2 == 0) {
+                binding.textView.setText("Result = Undefined!");
+            }
+            else {
 
-            textView.setText("Result = "+result);
+                result = number1 / number2;
+                result = Double.parseDouble(new DecimalFormat("##.###").format(result));
+
+                binding.textView.setText("Result = " + result);
+            }
 
         }
     }
