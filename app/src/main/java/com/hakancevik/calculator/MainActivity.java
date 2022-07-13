fe84278941cdc77e51a93679435daf8b09124bb2
Hakan Çevik <hakanxcevik7@gmail.com>
2022-07-13 15:50:43 +0300

    design fix

--- app/src/main/java/com/hakancevik/calculator/MainActivity.java
+++ app/src/main/java/com/hakancevik/calculator/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    double number1, number2, result;
+    double number1, number2, result=0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     public void subtraction(View view) {
         if (binding.number1Text.getText().toString().matches("") || binding.number2Text.getText().toString().matches("")) {
-            binding.textView.setText("Please, Enter Number!");
+            binding.textView.setText("Please, Enter a Number!");
         }
         else {
             number1 = Double.parseDouble(binding.number1Text.getText().toString());
@@ -93,16 +93,16 @@ public class MainActivity extends AppCompatActivity {
             number1 = Double.parseDouble(binding.number1Text.getText().toString());
             number2 = Double.parseDouble(binding.number2Text.getText().toString());
 
-            if (number1 != 0 && number2 == 0) {
+            if (number1 !=0 && number2 !=0 ){
+                result = number1 / number2;
+                binding.textView.setText("Result = " + result);
+
+            }
+
+            else if (number1 != 0) {
                 binding.textView.setText("Result = Undefined!");
             }
-            else {
 
-                result = number1 / number2;
-                result = Double.parseDouble(new DecimalFormat("##.###").format(result));
-
-                binding.textView.setText("Result = " + result);
-            }
 
         }
     }
